e804386518df5945fbdb7a51ab0e91889291e567
churiharsh <harsh.201904111@vcet.edu.in>
2022-02-22 17:05:10 +0530

    sorted

--- proctor/proctor/settings.py
+++ proctor/proctor/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'proctor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'adminsite/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
